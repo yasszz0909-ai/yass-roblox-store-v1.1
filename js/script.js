f8cf1c6c852a1de1ecc72be61aaa3c861a785340
yasszz0909-ai <yasszz0909@gmail.com>
2025-12-29 19:55:36 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,8 +63,7 @@ function sendToWA() {
                     `🔢 Jumlah: ${qty}\n` +
                     `💳 Pembayaran: ${payment}\n` +
                     `💰 *Total: ${total}*\n\n` +
-                    `Mohon segera berikan payment nya.` +
-                    `Saya akan segera mengirimkan bukti transfer.';
+                    `Saya akan segera mengirimkan bukti transfer.`;
 
     const waUrl = `https://wa.me/${WA_NUMBER}?text=${encodeURIComponent(message)}`;
     window.open(waUrl, '_blank');

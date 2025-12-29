cf783acde5cd8b08a09249793429dd7212d039f5
yasszz0909-ai <yasszz0909@gmail.com>
2025-12-29 19:47:54 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -4,8 +4,8 @@ const products = [
     { id: 2, name: "100 Robux (Gamepass)", price: 16000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
     { id: 3, name: "500 Robux (Gamepass)", price: 78000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
     { id: 4, name: "1000 Robux (Gamepass)", price: 150000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
-    { id: 5, name: "Redfinger VIP 7 Day", price: 22000, img: "rf.png", note: "Code Redeem Redfinger", limited: false },
-    { id: 6, name: "Redfinger VIP 30 Day", price: 73000, img: "rf.png", note: "Code Redeem Redfinger", limited: false },
+    { id: 5, name: "Redfinger VIP 7 Day", price: 22000, img: "rf.png", note: "Code Redeem Redfinger. *NOTE:⚠️ISI BAGIAN USERNAME ROBLOX DENGAN EMAIL/NOMOR WA ANDA!", limited: false },
+    { id: 6, name: "Redfinger VIP 30 Day", price: 73000, img: "rf.png", note: "Code Redeem Redfinger. *NOTE:⚠️ISI BAGIAN USERNAME ROBLOX DENGAN EMAIL/NOMOR WA ANDA!", limited: false },
     { id: 7, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "Email bisa diganti", limited: false },
     { id: 8, name: "Akun Ghostfinn", price: 18000, img: "person.png", note: "Akun Polosan Rod", limited: false },
     { id: 9, name: "Joki AFK 1 Jam", price: 700, img: "time.png", note: "Joki Map Fish It!", limited: false },
@@ -15,4 +15,5 @@ const products = [
     { id: 13, name: "Evolved Enchant Stone", price: 1100, img: "e-stone.png", note: "Trade In-Game", limited: true },
     { id: 14, name: "Secret Tumbal", price: 4000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
 ];
+
 

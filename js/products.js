98ad514055d96a3cd5a6f85da0a7c7d98bf08c53
yasszz0909-ai <yasszz0909@gmail.com>
2025-12-29 19:29:59 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -4,12 +4,15 @@ const products = [
     { id: 2, name: "100 Robux (Gamepass)", price: 16000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
     { id: 3, name: "500 Robux (Gamepass)", price: 78000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
     { id: 4, name: "1000 Robux (Gamepass)", price: 150000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
-    { id: 5, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "Email bisa diganti", limited: false },
-    { id: 6, name: "Akun Ghostfinn", price: 18000, img: "person.png", note: "Akun Polosan Rod", limited: false },
-    { id: 7, name: "Joki AFK 1 Jam", price: 700, img: "time.png", note: "Joki Map Fish It!", limited: false },
-    { id: 8, name: "Joki AFK 24 Jam", price: 11000, img: "time.png", note: "Paket Hemat 24 Jam", limited: false },
-    { id: 9, name: "El Shark Gran Maja", price: 13000, img: "el-shark.png", note: "Trade In-Game", limited: true },
-    { id: 10, name: "Enchant Stone x10", price: 1000, img: "stone.png", note: "Trade In-Game", limited: true },
-    { id: 11, name: "Evolved Enchant Stone", price: 1100, img: "e-stone.png", note: "Trade In-Game", limited: true },
-    { id: 12, name: "Secret Tumbal", price: 4000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
+    { id: 5, name: "Redfinger VIP 7 Day", price: 22000, img: "rf.png", note: "Code Redeem Redfinger", limited: false },
+    { id: 6, name: "Redfinger VIP 30 Day", price: 73000, img: "rf.png", note: "Code Redeem Redfinger", limited: false },
+    { id: 7, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "Email bisa diganti", limited: false },
+    { id: 8, name: "Akun Ghostfinn", price: 18000, img: "person.png", note: "Akun Polosan Rod", limited: false },
+    { id: 9, name: "Joki AFK 1 Jam", price: 700, img: "time.png", note: "Joki Map Fish It!", limited: false },
+    { id: 10, name: "Joki AFK 24 Jam", price: 11000, img: "time.png", note: "Paket Hemat 24 Jam", limited: false },
+    { id: 11, name: "El Shark Gran Maja", price: 13000, img: "el-shark.png", note: "Trade In-Game", limited: true },
+    { id: 12, name: "Enchant Stone x10", price: 1000, img: "stone.png", note: "Trade In-Game", limited: true },
+    { id: 13, name: "Evolved Enchant Stone", price: 1100, img: "e-stone.png", note: "Trade In-Game", limited: true },
+    { id: 14, name: "Secret Tumbal", price: 4000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
 ];
+

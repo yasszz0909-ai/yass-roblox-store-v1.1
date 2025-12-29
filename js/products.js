941194aa935d085e9b7e6d91ec209e0bab09d29b
yasszz0909-ai <yasszz0909@gmail.com>
2025-12-29 16:57:31 +0700

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,15 @@
+// Daftar Produk Yass Store
+const products = [
+    { id: 1, name: "1 Robux (Gamepass)", price: 170, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
+    { id: 2, name: "100 Robux (Gamepass)", price: 16000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
+    { id: 3, name: "500 Robux (Gamepass)", price: 78000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
+    { id: 4, name: "1000 Robux (Gamepass)", price: 150000, img: "robux.png", note: "Pending 5-7 Hari", limited: false },
+    { id: 5, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "Email bisa diganti", limited: false },
+    { id: 6, name: "Akun Ghostfinn", price: 18000, img: "person.png", note: "Akun Polosan Rod", limited: false },
+    { id: 7, name: "Joki AFK 1 Jam", price: 700, img: "time.png", note: "Joki Map Fish It!", limited: false },
+    { id: 8, name: "Joki AFK 24 Jam", price: 11000, img: "time.png", note: "Paket Hemat 24 Jam", limited: false },
+    { id: 9, name: "El Shark Gran Maja", price: 13000, img: "el-shark.png", note: "Trade In-Game", limited: true },
+    { id: 10, name: "Enchant Stone x10", price: 1000, img: "stone.png", note: "Trade In-Game", limited: true },
+    { id: 11, name: "Evolved Enchant Stone", price: 1100, img: "e-stone.png", note: "Trade In-Game", limited: true },
+    { id: 12, name: "Secret Tumbal", price: 4000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
+];

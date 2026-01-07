52e88c3ca1aec30575d2df4c00c6988d9086fccd
yasszz0909-ai <yasszz0909@gmail.com>
2026-01-07 19:55:11 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -14,12 +14,13 @@ const products = [
     { id: 12, name: "Joki Diamond Rod", price: 350000, img: "time.png", note: "Joki Sampai Dapat Diamond Rod, Minimal Element Rod", limited: false },
     { id: 13, name: "Joki AFK 1 Jam", price: 700, img: "time.png", note: "Joki Map Fish It!", limited: false },
     { id: 14, name: "Joki AFK 24 Jam", price: 11000, img: "time.png", note: "Paket Hemat 24 Jam", limited: false },
-    { id: 15, name: "El Shark Gran Maja", price: 13000, img: "el-shark.png", note: "Trade In-Game", limited: true },
-    { id: 16, name: "Robot Kraken", price: 8000, img: "kraken.png", note: "Trade In-Game", limited: true },
+    { id: 15, name: "El Shark Gran Maja", price: 10000, img: "el-shark.png", note: "Trade In-Game", limited: true },
+    { id: 16, name: "Robot Kraken", price: 6000, img: "kraken.png", note: "Trade In-Game", limited: true },
     { id: 17, name: "Enchant Stone x10", price: 1000, img: "stone.png", note: "Trade In-Game", limited: true },
-    { id: 18, name: "Evolved Enchant Stone", price: 1100, img: "e-stone.png", note: "Trade In-Game", limited: true },
-    { id: 19, name: "Secret Tumbal", price: 4000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
+    { id: 18, name: "Evolved Enchant Stone", price: 1000, img: "e-stone.png", note: "Trade In-Game", limited: true },
+    { id: 19, name: "Secret Tumbal", price: 2000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
 ];
+
 
 
 

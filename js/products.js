2c00c8f34493c90c7b4de7982669693fd6522f32
yasszz0909-ai <yasszz0909@gmail.com>
2026-01-08 13:52:00 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -6,8 +6,8 @@ const products = [
     { id: 4, name: "1000 Robux (Gamepass)", price: 150000, img: "robux.png", note: "|Stok : 5K+| Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
     { id: 5, name: "Redfinger VIP 7 Day", price: 22000, img: "rf.png", note: "Code Redeem Redfinger. *NOTE:⚠️ISI BAGIAN USERNAME ROBLOX DENGAN EMAIL/NOMOR WA ANDA!", limited: false },
     { id: 6, name: "Redfinger VIP 30 Day", price: 64000, img: "rf.png", note: "Code Redeem Redfinger. *NOTE:⚠️ISI BAGIAN USERNAME ROBLOX DENGAN EMAIL/NOMOR WA ANDA!", limited: false },
-    { id: 7, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "Email bisa diganti", limited: false },
-    { id: 8, name: "Akun Ghostfinn", price: 18000, img: "person.png", note: "Akun Polosan Rod", limited: false },
+    { id: 7, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "|Isi Bagian Username Roblox Dengan Nomor WhatsApp| Email bisa diganti", limited: false },
+    { id: 8, name: "Akun Ghostfinn", price: 18000, img: "person.png", note: "|Isi Bagian Username Roblox Dengan Nomor WhatsApp| Akun Polosan Rod", limited: false },
     { id: 9, name: "Joki Ghostfinn Rod", price: 19000, img: "time.png", note: "Joki Sampai Dapat Ghostfinn Rod, Minimal Ares Rod", limited: false },
     { id: 10, name: "Joki Ghostfinn Rod", price: 22000, img: "time.png", note: "Joki Sampai Dapat Ghostfinn Rod, Minimal Astral Rod", limited: false },
     { id: 11, name: "Joki Element Rod", price: 62000, img: "time.png", note: "Joki Sampai Dapat Element Rod, Minimal Ghostfinn Rod", limited: false },
@@ -20,6 +20,7 @@ const products = [
     { id: 18, name: "Evolved Enchant Stone", price: 1000, img: "s.png", note: "|Stok : 0| Trade In-Game", limited: true },
     { id: 19, name: "Secret Tumbal", price: 2000, img: "secret-tumbal.png", note: "|Stok : 4|Trade In-Game", limited: true }
 ];
+
 
 
 

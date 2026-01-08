44669dd2b4677c6a2e187413761bc40ff3f2cb3f
yasszz0909-ai <yasszz0909@gmail.com>
2026-01-08 13:20:54 +0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,9 +1,9 @@
 // Daftar Produk Yass Store
 const products = [
-    { id: 1, name: "1 Robux (Gamepass)", price: 170, img: "robux.png", note: "Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
-    { id: 2, name: "100 Robux (Gamepass)", price: 16000, img: "robux.png", note: "Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
-    { id: 3, name: "500 Robux (Gamepass)", price: 78000, img: "robux.png", note: "Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
-    { id: 4, name: "1000 Robux (Gamepass)", price: 150000, img: "robux.png", note: "Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
+    { id: 1, name: "1 Robux (Gamepass)", price: 170, img: "robux.png", note: "|Stok : 5K+| Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
+    { id: 2, name: "100 Robux (Gamepass)", price: 16000, img: "robux.png", note: "|Stok : 5K+| Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
+    { id: 3, name: "500 Robux (Gamepass)", price: 78000, img: "robux.png", note: "|Stok : 5K+| Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
+    { id: 4, name: "1000 Robux (Gamepass)", price: 150000, img: "robux.png", note: "|Stok : 5K+| Via Gamepass(After Tax) Pending 5-7 Hari", limited: false },
     { id: 5, name: "Redfinger VIP 7 Day", price: 22000, img: "rf.png", note: "Code Redeem Redfinger. *NOTE:⚠️ISI BAGIAN USERNAME ROBLOX DENGAN EMAIL/NOMOR WA ANDA!", limited: false },
     { id: 6, name: "Redfinger VIP 30 Day", price: 64000, img: "rf.png", note: "Code Redeem Redfinger. *NOTE:⚠️ISI BAGIAN USERNAME ROBLOX DENGAN EMAIL/NOMOR WA ANDA!", limited: false },
     { id: 7, name: "Akun Roblox VC", price: 43000, img: "person.png", note: "Email bisa diganti", limited: false },
@@ -14,12 +14,13 @@ const products = [
     { id: 12, name: "Joki Diamond Rod", price: 350000, img: "time.png", note: "Joki Sampai Dapat Diamond Rod, Minimal Element Rod", limited: false },
     { id: 13, name: "Joki AFK 1 Jam", price: 700, img: "time.png", note: "Joki Map Fish It!", limited: false },
     { id: 14, name: "Joki AFK 24 Jam", price: 11000, img: "time.png", note: "Paket Hemat 24 Jam", limited: false },
-    { id: 15, name: "El Shark Gran Maja", price: 10000, img: "el-shark.png", note: "Trade In-Game", limited: true },
-    { id: 16, name: "Robot Kraken", price: 6000, img: "kraken.png", note: "Trade In-Game", limited: true },
-    { id: 17, name: "Enchant Stone x10", price: 1000, img: "stone.png", note: "Trade In-Game", limited: true },
+    { id: 15, name: "El Shark Gran Maja", price: 10000, img: "el-shark.png", note: "|Stok : 2| Trade In-Game", limited: true },
+    { id: 16, name: "Robot Kraken", price: 6000, img: "kraken.png", note: "|Stok : 2| Trade In-Game", limited: true },
+    { id: 17, name: "Enchant Stone x10", price: 1000, img: "stone.png", note: "|Stok : 1000| Trade In-Game", limited: true },
     { id: 18, name: "Evolved Enchant Stone", price: 1000, img: "s.png", note: "Trade In-Game", limited: true },
-    { id: 19, name: "Secret Tumbal", price: 2000, img: "secret-tumbal.png", note: "Trade In-Game", limited: true }
+    { id: 19, name: "Secret Tumbal", price: 2000, img: "secret-tumbal.png", note: "|Stok : 4|Trade In-Game", limited: true }
 ];
+
 
 
 
